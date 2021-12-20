d6a70772ebbd9e938580a68654b43b35a2f955af
fisal77 <fisal0707@gmail.com>
2021-12-21 02:23:06 +0300

    Add frontend files

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder='static')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/home")
+@app.route("/index")
+def index():
+    return render_template('pages/home.html')
 
 
 if __name__ == '__main__':
